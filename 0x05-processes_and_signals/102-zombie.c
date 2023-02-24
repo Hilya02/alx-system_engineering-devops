98557e1a3b5fcedf3ea11585304188d3eaf3960a
Hilya Ntinda <113698301+Hilya02@users.noreply.github.com>
2023-02-24 22:32:07 +0200

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,34 +1,39 @@
-#!/usr/bin/env bash
-#Bash script that creates a subprocess
-. /lib/lsb/init-functions
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-PIDFILE=/var/run/my_process.pid
-
-start() {
-    ./manage_my_process &
-    echo "$!" > "$PIDFILE"
+/**
+ * infinite_while - function that containts an infinite loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
-stop() {
-    sudo kill -15 "$(cat "$PIDFILE")" && rm -f "$PIDFILE"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			dprintf(1, "Zombie process created, PID: %d\n", getpid());
+			return (0);
+		}
+	}
+	infinite_while();
+	return (0);
 }
-
-case "$1" in 
-    start)
-       start
-       echo "manage_my_process started"
-       ;;
-    stop)
-       stop
-       echo "manage_my_process stopped"
-       ;;
-    restart)
-       stop
-       start
-       echo "manage_my_process restarted"
-       ;;
-    *)
-       echo "Usage: manage_my_process {start|stop|restart}"
-esac
-
-exit 0
